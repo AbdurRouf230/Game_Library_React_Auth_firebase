b5da8be0beccc386cd41a0b3cc17ceb88cf426a1
AbdurRouf230 <abdur230rouf@gmail.com>
2026-05-11 15:20:02 +0600

    Navbar and routes are completed

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import { Outlet } from "react-router";
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <Navbar></Navbar>
+      </nav>
+      <main>
+        <Outlet></Outlet>
+      </main>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div>
+      <aside>
+        <h1>This is side bar</h1>
+      </aside>
+      <h1>This is Home page Body</h1>
+    </div>
+  );
+};
+export default Home;
